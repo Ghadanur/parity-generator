5d0891eb95f334caf2f3cd0b5d7b7a5116f751e6
Ghada <ghadanoor.el@gmail.com>
2025-07-26 22:55:54 +0500

    Update parity_generator_tb.v

--- parity_generator_tb.v
+++ parity_generator_tb.v
@@ -8,14 +8,14 @@ parity_generator uut(
     );
 //TestSequences
 initial begin
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=0;data_in[14]=0;data_in[15]=0;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=0;data_in[14]=0;data_in[15]=1;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=0;data_in[14]=1;data_in[15]=0;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=0;data_in[14]=1;data_in[15]=1;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=1;data_in[14]=0;data_in[15]=0;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=1;data_in[14]=0;data_in[15]=1;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=1;data_in[14]=1;data_in[15]=0;#10;
-    data_in[0]=0;data_in[1]=0;data_in[2]=0;data_in[3]=0;data_in[4]=0;data_in[5]=0;data_in[6]=0;data_in[7]=0;data_in[8]=0;data_in[9]=0;data_in[10]=0;data_in[11]=0;data_in[12]=0;data_in[13]=1;data_in[14]=1;data_in[15]=1;#10;
+        data_in = 16'b0000_0000_0000_0000; #10;
+        data_in = 16'b0000_0000_0000_0001; #10;
+        data_in = 16'b0000_0000_0000_0010; #10;
+        data_in = 16'b0000_0000_0000_0011; #10;
+        data_in = 16'b0000_0000_0000_0100; #10;
+        data_in = 16'b0000_0000_0000_0101; #10;
+        data_in = 16'b0000_0000_0000_0110; #10;
+        data_in = 16'b0000_0000_0000_0111; #10;
     $finish;
 end
 endmodule
